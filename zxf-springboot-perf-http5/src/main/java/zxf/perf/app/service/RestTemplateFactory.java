79218133c1f6b0aea0a288cf58872a085c55bc65
Davis <zanxiaofeng@163.com>
2025-12-18 15:34:38 +0800

    upodate

--- zxf-springboot-perf-http5/src/main/java/zxf/perf/app/service/RestTemplateFactory.java
+++ zxf-springboot-perf-http5/src/main/java/zxf/perf/app/service/RestTemplateFactory.java
@@ -1,8 +1,10 @@
 package zxf.perf.app.service;
 
 import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.config.ConnectionConfig;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.core5.util.TimeValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -23,8 +25,15 @@ public class RestTemplateFactory {
     }
 
     public RestTemplate newRestTemplateWithCustomHttpClientWithPool() throws Exception {
+        PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+                .setDefaultConnectionConfig(ConnectionConfig.custom()
+                        .setTimeToLive(TimeValue.ofMinutes(1)).build())
+                .setMaxConnTotal(100)
+                .setMaxConnPerRoute(20)
+                .build();
+
         HttpClient httpClient = HttpClients.custom()
-                .setConnectionManager(new PoolingHttpClientConnectionManager())
+                .setConnectionManager(connectionManager)
                 .evictIdleConnections(TimeValue.ofSeconds(30))
                 .build();
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
